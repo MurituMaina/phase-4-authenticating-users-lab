0317a95e1e681065901b5c65685acf9ebf384edf
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-30 00:19:47 +0300

    completed assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,12 @@
 class SessionsController < ApplicationController
-    def create
-        user = User.find_by(username: params[:username])
-        session[:user_id]= user.id
-        render json: user
-    end
+  def create
+    user = User.find_by(username: params[:username])
+    session[:user_id] = user.id
+    render json: user
+  end
 
-    def destroy
-        # user = User.find_by(:user_id params[:user_id])
-        # user.destroy
-        session.delete :user_id
-         head :no_content
-
-    end
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
 end
